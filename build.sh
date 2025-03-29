d28eb7cfc55baaca0ecc8d3b4a4405dfed439962
Serhii Doroshenkov <serhii.doroshenkov@spryker.com>
2025-03-29 14:09:47 +0100

    cs fix, see SC1113

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#/bin/sh
+#! /bin/sh
 
 mkdir -p MutagenMon.app/Contents/MacOS
 GOOS=darwin CGO_ENABLED=1 go build -buildmode=pie  go.andmed.org/mutagenmon/cmd/mutagenmon && mv mutagenmon MutagenMon.app/Contents/MacOS/
